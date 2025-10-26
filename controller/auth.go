d42ddb8e5a1146ae11b8b670bde32c9aafaeef9f
陈冠余 <1150542638@qq.com>
2025-10-26 11:58:49 +0800

    feat(login): implement login

--- controller/auth.go
+++ controller/auth.go
@@ -1,11 +1,40 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"template/common"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Auth struct{}
 
 func (*Auth) Login(c *gin.Context) {
-
+	var json struct {
+		Name     string `json:"name" binding:"required"`
+		Password string `json:"password" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.Error(common.ErrNew(err, common.ParamErr))
+		return
+	}
+	userInfo, err := srv.Auth.Login(json.Name, json.Password)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+	SessionSet(c, "user-session", UserSession{
+		ID:       userInfo.ID,
+		UserName: userInfo.Name,
+		Level:    1,
+	})
+	c.JSON(http.StatusOK, ResponseNew(c, struct {
+		ID   int64  `json:"id"`
+		Name string `json:"name"`
+	}{
+		ID:   userInfo.ID,
+		Name: userInfo.Name,
+	}))
 }
 
 func (*Auth) Logout(c *gin.Context) {
--- service/auth.go
+++ service/auth.go
@@ -1,3 +1,25 @@
 package service
 
+import (
+	"fmt"
+	"template/common"
+	"template/model"
+	"template/utils"
+)
+
 type Auth struct{}
+
+func (a Auth) Login(name string, password string) (model.User, error) {
+	var user model.User
+	if err := model.DB.Where("name = ?", name).First(&user).Error; err != nil {
+		return model.User{}, common.ErrNew(fmt.Errorf("查询用户信息失败：%v", err), common.SysErr)
+	}
+	decrypted, decryptErr := utils.Decrypt(user.Password)
+	if decryptErr != nil {
+		return model.User{}, common.ErrNew(fmt.Errorf("解密密码时出错：%v", decryptErr), common.SysErr)
+	}
+	if decrypted != password {
+		return model.User{}, common.ErrNew(fmt.Errorf("密码错误"), common.AuthErr)
+	}
+	return user, nil
+}
