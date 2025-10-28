42317951bd495bcdb1dfe2813a82575160f2e77f
陈冠余 <1150542638@qq.com>
2025-10-28 18:37:12 +0800

    fix(user): ignore password when serializing json

--- model/user.go
+++ model/user.go
@@ -3,5 +3,5 @@ package model
 type User struct {
 	BaseModel
 	Name     string `gorm:"column:name;comment:'昵称'" json:"name"`
-	Password string `gorm:"column:password;comment:'密码'" json:"password"`
+	Password string `gorm:"column:password;comment:'密码'" json:"-"`
 }
