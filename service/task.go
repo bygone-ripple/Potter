04ae030fc683da33a2dc2118b2b811e6fe1b1c36
陈冠余 <1150542638@qq.com>
2025-10-29 20:32:36 +0800

    fix(task): prevent race condition in task delete

--- service/task.go
+++ service/task.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"errors"
 	"fmt"
 	"template/common"
 	"template/model"
@@ -35,19 +34,12 @@ func (t Task) GetInfo(taskID int) (model.Task, error) {
 }
 
 func (t Task) Delete(taskID int, userID int64) error {
-	var record model.Task
-	if err := model.DB.Model(&model.Task{}).Where("id = ?", taskID).Select("id, poster_id").First(&record).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return common.ErrNew(fmt.Errorf("未查询到该锅单"), common.OpErr)
-		}
-		return common.ErrNew(fmt.Errorf("查询锅单失败：%v", err), common.SysErr)
+	res := model.DB.Model(&model.Task{}).Where("id = ? AND poster_id = ?", taskID, userID).Delete(&model.Task{})
+	if res.Error != nil {
+		return common.ErrNew(fmt.Errorf("删除锅单失败：%v", res.Error), common.SysErr)
 	}
-	// 仅发布者可删除锅单，如果没有发布者...说明数据库被入侵了，删了算了
-	if record.PosterID != nil && *record.PosterID != userID {
-		return common.ErrNew(fmt.Errorf("无权限删除该锅单"), common.AuthErr)
-	}
-	if err := model.DB.Model(&model.Task{}).Where("id = ?", taskID).Delete(&model.Task{}).Error; err != nil {
-		return common.ErrNew(fmt.Errorf("删除锅单失败：%v", err), common.SysErr)
+	if res.RowsAffected == 0 {
+		return common.ErrNew(fmt.Errorf("该锅单不存在或负责人不是您"), common.OpErr)
 	}
 
 	return nil
