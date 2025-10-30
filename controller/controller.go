0d2164709f459dc8d23b6cd3c381ee30b7f4dee1
陈冠余 <1150542638@qq.com>
2025-10-30 18:53:15 +0800

    feat(upload): implement uploading files

--- controller/controller.go
+++ controller/controller.go
@@ -1,6 +1,14 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"template/common"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Controller struct {
 	Auth
@@ -16,5 +24,24 @@ func New() *Controller {
 
 // Upload 上传图片、视频等文件到本地，存储在 uploads 目录下
 func (*Controller) Upload(c *gin.Context) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.Error(common.ErrNew(fmt.Errorf("获取文件失败：%v", err), common.ParamErr))
+		return
+	}
+	// 新文件名：原文件名_时间戳.扩展名
+	ext := filepath.Ext(file.Filename)
+	name := file.Filename[:len(file.Filename)-len(ext)]
+	newFilename := fmt.Sprintf("%s_%d%s", name, time.Now().Unix(), ext)
 
+	savePath := filepath.Join("uploads", newFilename)
+	if err := c.SaveUploadedFile(file, savePath); err != nil {
+		c.Error(common.ErrNew(fmt.Errorf("保存文件失败：%v", err), common.SysErr))
+		return
+	}
+
+	uri := fmt.Sprintf("/static/%s", newFilename)
+	c.JSON(http.StatusOK, ResponseNew(c, gin.H{
+		"uri": uri,
+	}))
 }
