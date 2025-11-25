8604c2623d0fcbfbee4bd9c2f50e176b02cc1477
陈冠余 <1150542638@qq.com>
2025-11-25 12:36:49 +0800

    fix(user): set unique constraint for user name

--- model/user.go
+++ model/user.go
@@ -2,7 +2,7 @@ package model
 
 type User struct {
 	BaseModel
-	Name     string `gorm:"column:name;comment:'昵称'" json:"name"`
+	Name     string `gorm:"column:name;unique;comment:'昵称'" json:"name"`
 	Avatar   string `gorm:"column:avatar;comment:'头像'" json:"avatar"`
 	Password string `gorm:"column:password;comment:'密码'" json:"-"`
 }
